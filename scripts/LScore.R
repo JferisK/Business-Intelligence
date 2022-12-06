aab34ebc41e7186ea5d79b79bbb116a2c0a86492
nFromm <fromm.noah@gmail.com>
2022-12-06 18:24:55 +0100

    Use loadData() in LScore.R

--- scripts/LScore.R
+++ scripts/LScore.R
@@ -3,13 +3,9 @@ library(readr)
 library(party)
 
 #LScore
-set.seed(42)
-data <- read_csv("SmartBuild.csv")
-data$LScore <- as.factor(data$LScore)
-index <- sample(1:nrow(data),nrow(data)*0.8)
+source("scripts/LoadData.R")
 
-train_data <- data[index,]
-test_data <- data[-index,]
+loadData()
 
 model_LScore <- ctree(LScore~ Gewicht + Durchmesser + Hoehe,data=train_data)
 res_test <- predict(model_LScore,test_data)
--- scripts/LoadData.R
+++ scripts/LoadData.R
@@ -4,9 +4,18 @@ loadData <- function(ran_seed =42, train_split=0.8){
   data <- read_csv("./data/SmartBuild.csv")
   
   
-  #
-  index <- sample(1:nrow(data),nrow(data)*0.8)
+  #factoren setzen
+  data$LScore <- as.factor(data$LScore)
+  data$Fehler <- as.factor(data$Fehler)
+  data$XKlasse <- as.factor(data$XKlasse)
   
-  train_data <- data[index,]
-  test_data <- data[-index,]
+  
+  #Train/Test aufteilen
+  set.seed(ran_seed)
+  index <- sample(1:nrow(data),nrow(data)*train_split)
+  
+  complete_data <<- data[,]
+  train_data <<- data[index,]
+  test_data <<- data[-index,]
 }
+loadData()
