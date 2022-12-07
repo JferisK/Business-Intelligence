517fe4b97684e2bf93bdca6e78b0daf7b99097aa
nFromm <fromm.noah@gmail.com>
2022-12-07 10:44:06 +0100

    change order of imports

--- scripts/LoadLibraries.R
+++ scripts/LoadLibraries.R
@@ -1,7 +1,9 @@
 loadLibraries <- function(){
   
-  libraries <- c("readr","party","rgl")
+  #Pakete
+  libraries <- c("readr","party","rgl","ROCR","ISLR")
   
+  #Durch Pakete iteriern und aktivieren
   for (l in libraries){
     library(l,character.only = TRUE)
   }
