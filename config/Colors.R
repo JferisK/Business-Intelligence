fab614d2d89df4cbed5d43d59e6185b81eeffd9d
MalteSt224 <malte.strunz@web.de>
2022-12-07 12:18:49 +0100

    chnaged line to lineColor

--- config/Colors.R
+++ config/Colors.R
@@ -2,7 +2,7 @@ loadColors <- function(){
   
   #defining global colors
   orange <<- "#f39f00"
-  line <<- "#0000FF"
+  lineColor <<- "#0000FF"
   
   #transparenter Hindergrund (für die PP)
   par(bg=NA)
