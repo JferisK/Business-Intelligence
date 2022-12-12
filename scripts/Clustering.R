f075d275612f196e74cfeb2f0f0226158cdd31d1
NoFr02 <93268500+NoFr02@users.noreply.github.com>
2022-12-12 09:58:34 +0100

    Finish clustering

--- scripts/Clustering.R
+++ scripts/Clustering.R
@@ -1,4 +1,5 @@
 
-
-model <- kmeans(full_data[,c(2,3,4)],2)
-plot3d(full_data$Durchmesser,full_data$Hoehe,full_data$Gewicht,col=model$cluster)
+data <- scale(full_data[,c(2,3,4)])
+data <- data.frame(data)
+model <- kmeans(data,2)
+plot3d(data$Durchmesser,data$Hoehe,data$Gewicht,col=model$cluster)
